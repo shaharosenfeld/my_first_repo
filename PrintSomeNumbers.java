e5c8cbcaed0b20e0ed31a9209582de2d0a3c2dbd
shaharosenfeld <154363294+shaharosenfeld@users.noreply.github.com>
2023-12-19 21:23:30 +0200

    Add files via upload

---
+++ PrintSomeNumbers.java
@@ -0,0 +1,22 @@
+public class PrintSomeNumbers {
+    public static void main(String[] args) {
+        // Declares an integer variable and sets it to 0
+        int i = 0;
+        while (i < 6) {
+            // Prints i, and increments it
+            System.out.println(i);
+            i = i + 1;
+        }
+        System.out.println("done");
+    }
+}
+//error- the class name need to be declared
+//the capital D changed to d
+// error: unclosed string literal
+//error: cannot find symbol
+//while running- cant find main
+//error: cannot find symbol
+//error: cannot find symbol
+//error: ';' expected
+//error: reached end of file while parsing }
+//endlees loop
